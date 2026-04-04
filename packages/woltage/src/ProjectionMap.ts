fefa63829568a9b5790a47bedd017fcb0a8c78a0
wesone <github@wesone.de>
2026-04-04 20:36:24 +0200

    updated projection map to cache current active map until modification

--- packages/woltage/src/ProjectionMap.ts
+++ packages/woltage/src/ProjectionMap.ts
@@ -2,9 +2,13 @@ import Projection from './read/Projection.ts';
 
 export default class ProjectionMap
 {
-    idMap: Map<string, Projection> = new Map();
-    activeProjectionMap: Map<string, Projection> = new Map();
+    idMap = new Map<string, Projection>();
+    activeProjections = new Map<string, Projection>();
     isRunning = false;
+
+    #activeProjectionsGen = 0;
+    #activeProjectionsSnapshot: typeof this.activeProjections | null = null;
+    #activeProjectionsSnapshotGen = -1;
 
     async add(projection: Projection) {
         this.idMap.set(projection.id, projection);
@@ -14,26 +18,29 @@ export default class ProjectionMap
 
     setActive(projectionName: string, projectionVersion: number, force = false) {
         const projectionId = Projection.getId(projectionName, projectionVersion);
-        if(!this.idMap.has(projectionId))
+
+        const projection = this.idMap.get(projectionId);
+        if(!projection)
             throw new Error(`Unknown projection '${Projection.getDisplayName(projectionName, projectionVersion)}'`);
-        const projection = this.idMap.get(projectionId)!;
         if(!projection.isLiveTracking && !force)
             throw new Error(`Projection '${projection.getDisplayName()}' is not tracking live events.`);
 
-        const activeProjection = this.activeProjectionMap.get(projectionName);
+        const activeProjection = this.activeProjections.get(projectionName);
         if(activeProjection)
             activeProjection.isActive = false;
         projection.isActive = true;
-        this.activeProjectionMap.set(projectionName, projection);
+
+        this.activeProjections.set(projectionName, projection);
+        this.#activeProjectionsGen++;
     }
 
     getActive(projectionName: string) {
-        return this.activeProjectionMap.get(projectionName);
+        return this.activeProjections.get(projectionName);
     }
 
     get(projectionId: string): Projection | undefined;
     get(projectionName: string, projectionVersion: number): Projection | undefined;
-    get(nameOrId: string, projectionVersion?: number): Projection | undefined {
+    get(nameOrId: string, projectionVersion?: number) {
         const projectionId = typeof projectionVersion !== 'number'
             ? nameOrId
             : Projection.getId(nameOrId, projectionVersion);
@@ -50,8 +57,23 @@ export default class ProjectionMap
         {
             if(!force)
                 throw new Error(`Can't remove active projection '${projection.getDisplayName()}'.`);
-            this.activeProjectionMap.delete(projectionName);
+
+            this.activeProjections.delete(projectionName);
+            this.#activeProjectionsGen++;
         }
+
+        // If the projection was not active, it could still be used by a long running process,
+        // that got the reference from getActiveProjections()...
+        //
+        // setActive('projectionName', 1);
+        // const snapshot = getActiveProjections();
+        // const promise = longRunningProcess(snapshot);
+        // setActive('projectionName', 2);
+        // remove('projectionName', 1); // will stop projectionName@1 but longRunningProcess could still use it
+        // await promise;
+        //
+        // Maybe we should detect if it's safe to stop the projection.
+        // Everything that relies on GC (e.g. WeakRef, FinalizationRegistry, ...) should be avoided.
 
         await projection.stop();
         this.idMap.delete(projection.id);
@@ -65,5 +87,14 @@ export default class ProjectionMap
     async stop() {
         this.isRunning = false;
         await Promise.all([...this.idMap.values()].map(projection => projection.stop()));
+    }
+
+    getActiveProjections() {
+        if(this.#activeProjectionsSnapshotGen !== this.#activeProjectionsGen || this.#activeProjectionsSnapshot === null)
+        {
+            this.#activeProjectionsSnapshot = new Map(this.activeProjections);
+            this.#activeProjectionsSnapshotGen = this.#activeProjectionsGen;
+        }
+        return this.#activeProjectionsSnapshot;
     }
 }
